765c68bb34cd561e429291dde7626ab519e20c53
teachmetw <srobinlee.teachmetw@gmail.com>
2023-08-19 13:09:02 -0700

    Conf

--- test/test_api.py
+++ test/test_api.py
@@ -1,21 +1,6 @@
 import pytest
-from app import create_app
 from back.database import db
 from back.models import User
-
-
-@pytest.fixture
-def test_client():
-    app = create_app()
-    app.config["TESTING"] = True
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
-
-    with app.test_client() as test_client:
-        with app.app_context():
-            db.create_all()
-        yield test_client
-        with app.app_context():
-            db.drop_all()
 
 
 # Parametrized test for adding and updating lol_username
